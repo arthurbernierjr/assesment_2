86d2f4cbc26f6a4e143a6be03a9be0b7b7a1b58f
Jcasanova1990 <jeremykcasanova@gmail.com>
2024-02-06 21:08:54 -0500

    completed calculate cube, isavowel, gettwolengths, getmultiplelengths, maxiumofthreenumbers, printlongestword

--- solutions.js
+++ solutions.js
@@ -70,3 +70,71 @@ function printPrimes(limit) {
 
 const limit = 20
 printPrimes(limit)
+
+
+// calculateCube
+
+function calculateCube(number) {
+    const result = 3 * 6 * 9
+    return result
+}
+console.log(calculateCube(3))
+
+// isAVowel
+
+function isAVowel(character) {
+    const vowels = "aeiouAEIOU"
+    const isVowel = vowels.includes(character)
+    return isVowel
+}
+console.log(isAVowel("p"))
+
+// getTwoLengths
+
+function getTwoLengths(str1, str2) {
+    const lengths = [str1.length, str2.length]
+    return lengths
+}
+console.log(getTwoLengths("pizza", "time"))
+
+// getMultipleLengths
+
+function getMultipleLengths(arr) {
+    const lengths = arr.map(str => str.length)
+    return lengths
+}
+console.log(getMultipleLengths(["pie", "cake", "muffin", "cupcake"]))
+
+// maximumOfThreeNumbers
+
+function maximumOfThreeNumbers(num1, num2, num3) {
+    const max = Math.max(num1, num2, num3)
+    return max
+}
+console.log(maximumOfThreeNumbers(13, 20, 2))
+
+// printLongestWord
+
+function printLongestWord(words) {
+    const LongestWord = words.reduce((longest, current) => 
+        current.length > longest.length ? current : longest
+    )
+    return LongestWord
+}
+console.log(printLongestWord(["tie", "t-shirt", "pants"]))
+
+// transmogrifyNumbers
+
+
+
+// projectEulerProblem2
+
+
+
+// aNeedleInTheHaystack
+
+
+
+// sumThePositive
+
+
