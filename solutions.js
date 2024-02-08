2b425f364812ad553101f7b2f948036466c6f2a0
unknown <liliannekho@gmail.com>
2024-02-07 22:16:41 -0500

    verbal questions complete

---
+++ solutions.js
@@ -0,0 +1,7 @@
+/*Verbal Questions 
+ 1. What is the difference between a parameter and an argument?
+    A parameter is a function delared variable an argument is a value that is passed to a function or a method when calling it. An argument is the values passed by the parameters when the function is called.
+
+2. Within a function, what is the difference between return and console.log?
+    console.log is used to log messages to the console, it does not affect the program execution
+    return is used within a function to send a value back to where it was called from. The code will exit the function and return the value to the code that called the function with return.
