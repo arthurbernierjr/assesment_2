3c425507bef01b1946c55a44689fba81d4e7dd9c
unknown <liliannekho@gmail.com>
2024-02-07 22:30:19 -0500

    palindrome completed

--- solutions.js
+++ solutions.js
@@ -5,3 +5,27 @@
 2. Within a function, what is the difference between return and console.log?
     console.log is used to log messages to the console, it does not affect the program execution
     return is used within a function to send a value back to where it was called from. The code will exit the function and return the value to the code that called the function with return.
+*/
+
+//coding challenges 
+
+//Palindrome 
+
+//convert the input string to lowercase to ensure the comparison is case-insensitive
+function palindrome(str) {
+    let lowercaseStr = str.toLowerCase()
+//Split the string into an array of characters, reverse the array, and then join it back into a string.
+    let reversedStr = lowercaseStr.split('').reverse().join('')
+//Compare the original lowercase string with the reversed string.
+
+    if (lowercaseStr === reversedStr) {
+//If they are the same, the function should return true; otherwise, it returns false.
+        return true
+    } else {
+        return false
+    }
+}
+
+let result = (palindrome("racecar"))
+console.log(result)
+
