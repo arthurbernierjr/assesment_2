89d87aa12b4a37133d0cc82f918b878cfb31aa26
Bryce Sexton <brycesexton15@gmail.com>
2024-02-08 12:25:50 -0800

    calc the cube

--- solutions.js
+++ solutions.js
@@ -23,7 +23,7 @@ const array = [11, 13, 32, 1]
 console.log(sumArray(array))
 
 //PRIME
-function checkPrime (num) {
+function checkPrime (num) { //check
     if(num <= 1){
         return false
     }
@@ -35,3 +35,23 @@ function checkPrime (num) {
     return true
 }
 console. log (checkPrime (9))
+
+function printPrimes(limit) { //print
+    for (let i = 2; i <= limit; i++) {
+        if (checkPrime(i)) {
+            console.log(i)
+        }
+    }
+}
+
+const limit = 20
+printPrimes(limit)
+
+//CALCULATE THE CUBE
+function calculateCube(number) {
+    return number * number * number
+}
+
+const num = 5
+const cube = calculateCube(num)
+console.log(`${num} cubed is ${cube}`)
