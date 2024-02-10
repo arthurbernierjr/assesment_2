8e6427bd74e747363d43248a8249ee37636ae3fd
Juan Zelaya Castillo <zjuan4101@gmail.com>
2024-02-09 22:29:57 -0500

    completed Quick Sort

--- solutions.js
+++ solutions.js
@@ -679,3 +679,27 @@ console.log(mergeSort([8, 3, 9, 5, 2, 7, 1, 6])) // [1, 2, 3, 5, 6, 7, 8, 9]
 // Add remaining elements from right array, if any
 // return result
 
+// Quick Sort
+function quickSort(arr) {
+    if (arr.length <= 1) {
+        return arr
+    }
+    const pivot = arr[arr.length - 1]
+    const leftArr = []
+    const rightArr = []
+
+    for (let i = 0; i < arr.length - 1; i++) {
+        if (arr[i] < pivot) {
+            leftArr.push(arr[i])
+        } else {
+            rightArr.push(arr[i])
+        }
+    }
+    return [...quickSort(leftArr), pivot, ...quickSort(rightArr)]
+}
+console.log(quickSort([1, 7, 5, 4])) // [1, 4, 5, 7]
+
+// Base Case: If the array has one or no elements, it is already sorted
+// Choose Pivot: Select the last element as the pivot
+// Partition: Rearrange the array based on the pivot, for loop
+// Recursively Apply: Apply quick sort to the sub-arrays formed by dividing the array around the pivot
